f0cc7651b889e4680f1a740990f3a9f835431d0c
Syntax-Er0r <79960679+syntax-er0r@users.noreply.github.com>
2021-04-08 00:41:07 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
-IZIN=$( curl https://pastebin.com/raw/bMN6pSqm | grep $MYIP )
+IZIN=$( curl https://raw.githubusercontent.com/syntax-er0r/AutoScriptSSH/main/ipvps | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 echo -e "${green}IP Berhasil Didaftarkan${NC}"
 else
