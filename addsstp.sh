8bfe43ed06189cc74086d2846ec7ae7517ba3ba2
Syntax-Er0r <79960679+syntax-er0r@users.noreply.github.com>
2021-04-07 19:16:19 +0700

    Add files via upload

---
+++ addsstp.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+IP=$(wget -qO- ifconfig.co);
+pass2="vpn"
+psk="vpn"
+read -p "Username: " user
+read -p "Password: " pass
+read -p "Expired (days): " masaaktif
+exp=`date -d "$masaaktif days" +"%Y-%m-%d"` 
+(printf "1\n\n\n$pass2\n"; sleep 1; printf "Hub $psk\n"; sleep 1; printf "UserCreate $user\n\n\n\n"; sleep 1; printf "UserPasswordSet $user\n$pass\n$pass\n"; sleep 1; printf "UserExpiresSet $user\n$exp 00:00:00\n") | /usr/local/vpnserver/./vpncmd &> /dev/null
+echo -e "\n### $user $exp">>"/var/lib/premium-script/data-user-sstp"
+tgl=$(echo "$exp" | cut -d- -f3)
+bln=$(echo "$exp" | cut -d- -f2)
+cat << EOF >> /etc/crontab
+# BEGIN_SSTP $user
+*/1 0 $tgl $bln * root printf "$user" | xp-sstp
+# END_SSTP $user
+EOF
+service cron restart
+clear
+cat <<EOF
+
+================================
+SSTP VPN 
+Server IP     : $IP
+Host		  : ${domain}
+Username      : $user
+Password      : $pass
+Port          : 444
+Cert          : http://$IP:81/cert.zip
+Expired Until : $exp
+================================
+By LostServer
+EOF
