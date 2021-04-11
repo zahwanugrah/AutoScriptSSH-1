aaec1eba57174cf4d875b84c3e36b5aac815be9d
Syntax-Er0r <79960679+syntax-er0r@users.noreply.github.com>
2021-04-11 14:18:27 +0700

    Update addsstp.sh

--- addsstp.sh
+++ addsstp.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+source /var/lib/premium-script/ipvps.conf
+if [[ "$IP" = "" ]]; then
+domain=$(cat /etc/v2ray/domain)
+else
+domain=$IP
+fi
 IP=$(wget -qO- ifconfig.co);
 pass2="dg"
 psk="vpn"
